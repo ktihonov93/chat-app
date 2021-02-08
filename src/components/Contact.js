2103142150365b349882f71ca520f94c2cd4c48f
ktihonov93 <k_tihonov@mail.ru>
2021-02-09 02:57:13 +0300

    first commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,29 @@ import React from 'react';
 import './Contact.css';
 
 function Contact() {
-    return (
-      <div className="Contact">
-        <img
-          src="https://randomuser.me/api/portraits/women/80.jpg"
-          alt="avatar"
-        />
-        <div>
-          <div>
+  return (
+    <div className="Contact">
+      <img className="avatar"
+        src="https://randomuser.me/api/portraits/women/80.jpg"
+        alt="avatar"
+      />
+      <div>
+        <div className="name">
           Marion Woods
           </div>
-          <div>online</div>
+        <div className="status">
+        <div className="status-online">
+          </div>
+          <div className="status-offline">
+          </div>
+          <div className="status-text">
+            online
+          </div>
+          
         </div>
       </div>
-    );
-  }
-  
-  export default Contact;
+    </div>
+  );
+}
+
+export default Contact;
