1e24386aec01711a1f79cfe42247b9b84232d359
ktihonov93 <k_tihonov@mail.ru>
2021-02-08 19:48:20 +0300

    add files

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact() {
+    return (
+      <div className="Contact">
+        <img
+          src="https://randomuser.me/api/portraits/women/80.jpg"
+          alt="avatar"
+        />
+        <div>
+          <div>
+          Marion Woods
+          </div>
+          <div>online</div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Contact;
